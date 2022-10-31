55b5cf6c2ee7f90bd3b683bed813ab78b935a215
aljarma <Mj314159265!>
2022-10-31 17:20:13 +0100

    works with datasource jndi builder

--- src/main/java/com/jarmak/fargartest/FargartestApplication.java
+++ src/main/java/com/jarmak/fargartest/FargartestApplication.java
@@ -4,8 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringBootApplication//(exclude={DataSourceAutoConfiguration.class})
+//@ImportResource("classpath:application-context.xml")
 public class FargartestApplication {
 
 
--- src/main/java/com/jarmak/fargartest/TomcatConfiguration.java
+++ src/main/java/com/jarmak/fargartest/TomcatConfiguration.java
@@ -5,6 +5,7 @@ import org.apache.catalina.startup.Tomcat;
 import org.apache.tomcat.util.descriptor.web.ContextResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,15 @@ import javax.sql.DataSource;
 public class TomcatConfiguration {
 
     private static final Logger logger = LoggerFactory.getLogger(FargartestApplication.class);
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
 
     @Bean
     public TomcatServletWebServerFactory tomcatFactory() {
@@ -32,14 +42,14 @@ public class TomcatConfiguration {
 
                 // context
                 ContextResource resource = new ContextResource();
-                resource.setName("jdbc/myJndiResource");
-                resource.setProperty("jndi-name","jdbc/myJndiResource");
+                resource.setName("jdbc/CasperDataSource");
                 resource.setType(DataSource.class.getName());
-                resource.setProperty("driverClassName", "org.postgresql.Driver");
-
-                resource.setProperty("url", "jdbc:postgresql://hostname:port/dbname");
-                resource.setProperty("username", "username");
-                resource.setProperty("password", "password");
+                resource.setProperty("driverClassName", "oracle.jdbc.OracleDriver");
+                resource.setProperty("url", url);
+                resource.setProperty("username", username);
+                resource.setProperty("password", password);
+//                resource.setProperty("jndi-name","java:comp/env/jdbc/CasperDataSource");
+//                resource.setProperty("factory", "org.apache.tomcat.jdbc.pool.DataSourceFactory");
                 context.getNamingResources()
                         .addResource(resource);
             }
